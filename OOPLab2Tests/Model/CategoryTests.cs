5724eef22e126f57c0174f52ca0e2d3f6ecc5188
Tanya Belkina <tvbelkina1680@gmail.com>
2019-01-19 23:59:40 +0700

    Added class CategoryTests with tests

---
+++ OOPLab2Tests/Model/CategoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OOPLab2.Model;
+
+namespace OOPLab2Tests.Model
+{
+    [TestClass]
+    public class CategoryTests
+    {
+        private string title = "Category";
+
+        [TestMethod]
+        public void Title_IsNotNull()
+        {
+            Category category = new Category(title);
+            Assert.AreEqual(title, category.Title);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Title_IsNull()
+        {
+            Category category = new Category(null);
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Category category = new Category(title);
+
+            string rezult = category.ToString();
+
+            Assert.AreEqual(rezult, title);
+        }        
+    }
+}
